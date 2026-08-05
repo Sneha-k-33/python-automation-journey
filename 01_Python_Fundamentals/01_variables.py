11ca39a905c35ada46c5398f0d9e841c0f83bb05
Sneha K <sneeha36@gmail.com>
2026-08-05 17:51:07 +0530

    Add Python variables and employee details exercises

---
+++ 01_Python_Fundamentals/01_variables.py
@@ -0,0 +1,47 @@
+# Lesson 1 : Variables
+
+ship_name = "MV Ocean Star"
+imo_number = 9876543
+rest_hours = 10
+is_compliant = True
+
+print("Ship Name:", ship_name)
+print("IMO Number:", imo_number)
+print("Rest Hours:", rest_hours)
+print("Is Compliant:", is_compliant)
+
+# Lesson 1 - Variables
+
+ship_name = "MVS Ocean Explorer"
+captain_name = "Captain Amar"
+imo_number = 456543
+voyage_number = 190
+port_of_departure = "Mumbai"
+port_of_arrival = "China"
+total_crew = 15
+
+print("Ship Name:", ship_name)
+print("Captain Name:", captain_name)
+print("IMO Number:", imo_number)
+print("Voyage Number:", voyage_number)
+print("Port of Departure:", port_of_departure)
+print("Port of Arrival:", port_of_arrival)
+print("Total Crew:", total_crew)
+
+employee_id = 4325
+employee_name = "Trishal Singh"
+designation = "Chief Officer"
+department = "Deck"
+ship_name = "MVS Ocean Explorer"
+experience_years = 10
+salary = 1500
+is_active = True
+
+print("Employee ID:", employee_id)
+print("Employee Name:", employee_name)
+print("Designation:", designation)
+print("Department:", department)
+print("Ship Name:", ship_name)
+print("Experience (Years):", experience_years)
+print("Salary:", salary)
+print("Is Active:", is_active)
